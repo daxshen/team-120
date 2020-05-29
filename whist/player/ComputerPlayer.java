56f8b9a7cf4bc180d7cd0ed68ab2c22cd3ae8fe5
Unknown <daxshen93@gmail.com>
2020-05-29 16:41:22 +1000

    testCommit

--- whist/player/ComputerPlayer.java
+++ whist/player/ComputerPlayer.java
@@ -31,4 +31,5 @@ public class ComputerPlayer extends Player {
 	//------------------- Methods ------------------------
     //asdasdasdasdasdasdasdasdasdasdasd
     //asdasdasdasdsad
+    //asdasdasdasd
 }
